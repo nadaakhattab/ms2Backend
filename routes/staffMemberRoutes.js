a7ff944b72dbafd398f6e722c80bc063f7b1bacf
Nada <nadaakhattab@gmail.com>
2020-12-18 19:55:48 +0200

    post request in route

--- routes/staffMemberRoutes.js
+++ routes/staffMemberRoutes.js
@@ -31,7 +31,7 @@ router.get('/viewProfile',async (req,res)=>{
     }
 });
 
-router.get('/updateProfile',async(req,res)=>{
+router.post('/updateProfile',async(req,res)=>{
     try{
         var userId=req.headers.payload.id;
         var inputMobile=req.body.mobileNumber;
