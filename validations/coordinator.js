5e66557974f1af9f2f5173dca399b6a843a8f305
unknown <aliakamel1999@gmail.com>
2020-12-22 22:03:42 +0200

    coordinator validation

---
+++ validations/coordinator.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+module.exports= {
+    addSlot:Joi.object({
+    slot:Joi.string().required(),
+    day:Joi.string().required(),
+    location:Joi.string().required(),
+    course: Joi.string().required(),
+    instructor: Joi.string().required()
+       
+      },
+   ),
+
+   replyRequests:Joi.object({
+    slotId:Joi.number().required(),
+    fromId:Joi.string().required(),
+    status:Joi.string().required(),
+    toId:Joi.string().required(),
+    type:Joi.string().required()
+      },
+   ),
+
+}
